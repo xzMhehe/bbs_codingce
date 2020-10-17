642cc00453f9cc7454a5c42ee0b37edd902af48d
MaXinze <2460798168@qq.com>
2020-10-17 11:10:45 +0800

    A 代码自动生成器

--- src/main/java/cn/com/codingce/generator/CodeGenerator.java
+++ src/main/java/cn/com/codingce/generator/CodeGenerator.java
@@ -22,9 +22,11 @@ public class CodeGenerator {
 
         // 2、全局配置
         GlobalConfig gc = new GlobalConfig();
+        //当前的项目路径
         String projectPath = System.getProperty("user.dir");
+        //所有代码都会生成到     /src/main/java  路径下
         gc.setOutputDir(projectPath + "/src/main/java");
-        gc.setAuthor("coding");
+        gc.setAuthor("小马Coding");
         gc.setOpen(false); //生成后是否打开资源管理器
         gc.setFileOverride(false); //重新生成时文件是否覆盖
         gc.setServiceName("%sService");	//去掉Service接口的首字母I
@@ -36,35 +38,44 @@ public class CodeGenerator {
 
         // 3、数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://localhost:3306/codingstudy?useUnicode=true&characterEncoding=utf-8&useSSL=false");
+        dsc.setUrl("jdbc:mysql://cdb-q9atzwrq.bj.tencentcdb.com:10167/codingstudy?useUnicode=true&characterEncoding=utf-8&useSSL=false");
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("root");
         dsc.setPassword("123456");
         dsc.setDbType(DbType.MYSQL);
+
         mpg.setDataSource(dsc);
 
         // 4、包配置
         PackageConfig pc = new PackageConfig();
-        pc.setModuleName("codingce");
-        pc.setParent("cn.com");
+        pc.setModuleName("blog");
+        pc.setParent("cn.com.codingce");
         pc.setController("controller");
         pc.setEntity("pojo");
         pc.setService("service");
         pc.setMapper("mapper");
+
         mpg.setPackageInfo(pc);
 
         // 5、策略配置
         StrategyConfig strategy = new StrategyConfig();
-        //strategy.setInclude("ks_\\w*");//设置要映射的表名
-        strategy.setInclude("ze_download");//设置要映射的表名
+        //strategy.setInclude("ze_user");//设置要映射的表名
+        //strategy.setInclude("ze_user", "ze_course");//可设置多个
+        strategy.setInclude("ze_user");//设置要映射的表名
         strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
         strategy.setTablePrefix("ze_");//设置表前缀不生成
-
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);//数据库表字段映射到实体的命名策略
         strategy.setEntityLombokModel(true); // lombok 模型 @Accessors(chain = true) setter链式操作
-
         strategy.setRestControllerStyle(true); //restful api风格控制器
         strategy.setControllerMappingHyphenStyle(true); //url中驼峰转连字符
+        //strategy.setLogicDeleteFieldName("deleted");    //逻辑删除字段
+        //自动填充配置
+        //TableFill gmtCreate = new TableFill("gmt_create", FieldFill.INSERT);
+        //TableFill gmtModified = new TableFill("gmt_modified", FieldFill.INSERT_UPDATE);
+        //ArrayList<TableFill> tableFills = new ArrayList<>();
+        //tableFills.add(gmtCreate);
+        //tableFills.add(gmtModified);
+        //strategy.setTableFillList(tableFills);
 
         mpg.setStrategy(strategy);
 
